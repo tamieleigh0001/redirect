feba0d401221e661b871e4a4664796222663e74b
tamieleigh0001 <156190168+tamieleigh0001@users.noreply.github.com>
2024-11-07 10:07:32 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+// Redirect to a different page
+header("Location: https://01df433d-05b6-4e6e-a8a4-f800cb9756e7-00-360x8klbtaazp.kirk.replit.dev");
+// Exit the script to prevent further execution
+exit();
+?>
